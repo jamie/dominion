98f5560b25ecc7ba54104a699c998842b0e48b4a
Jamie Macey <jamie.git@tracefunc.com>
2010-02-13 21:11:29 -0600

    stub definitions for non-special intrigue/seaside

--- lib/dominion/card/intrigue.rb
+++ lib/dominion/card/intrigue.rb
@@ -1,25 +1,36 @@
-Dominion::Card.define "Baron"
-Dominion::Card.define "Bridge"
-Dominion::Card.define "Conspirator"
-Dominion::Card.define "Coppersmith"
-Dominion::Card.define "Courtyard"
-Dominion::Card.define "Duke"
-Dominion::Card.define "Great Hall"
-Dominion::Card.define "Harem"
-Dominion::Card.define "Ironworks"
-Dominion::Card.define "Masquerade"
-Dominion::Card.define "Mining Village"
-Dominion::Card.define "Minion"
-Dominion::Card.define "Nobles"
-Dominion::Card.define "Pawn"
-Dominion::Card.define "Saboteur"
-Dominion::Card.define "Scout"
-Dominion::Card.define "Secret Chamber"
-Dominion::Card.define "Shanty Town"
-Dominion::Card.define "Steward"
-Dominion::Card.define "Swindler"
-Dominion::Card.define "Torturer"
-Dominion::Card.define "Trading Post"
-Dominion::Card.define "Tribute"
-Dominion::Card.define "Upgrade"
-Dominion::Card.define "Wishing Well"
+Dominion::Card.define_kingdom "Baron"
+Dominion::Card.define_kingdom "Bridge"
+Dominion::Card.define_kingdom "Conspirator"
+Dominion::Card.define_kingdom "Coppersmith"
+Dominion::Card.define_kingdom "Courtyard"
+Dominion::Card.define_kingdom "Duke"
+
+Dominion::Card.define_kingdom "Great Hall", 3, "Action", "Victory" do
+  vp 1
+  
+  extra_action 1
+  extra_card 1
+end
+
+Dominion::Card.define_kingdom "Harem", 6, "Treasure", "Victory" do
+  coins 2
+  vp 2
+end
+
+Dominion::Card.define_kingdom "Ironworks"
+Dominion::Card.define_kingdom "Masquerade"
+Dominion::Card.define_kingdom "Mining Village"
+Dominion::Card.define_kingdom "Minion"
+Dominion::Card.define_kingdom "Nobles"
+Dominion::Card.define_kingdom "Pawn"
+Dominion::Card.define_kingdom "Saboteur"
+Dominion::Card.define_kingdom "Scout"
+Dominion::Card.define_kingdom "Secret Chamber"
+Dominion::Card.define_kingdom "Shanty Town"
+Dominion::Card.define_kingdom "Steward"
+Dominion::Card.define_kingdom "Swindler"
+Dominion::Card.define_kingdom "Torturer"
+Dominion::Card.define_kingdom "Trading Post"
+Dominion::Card.define_kingdom "Tribute"
+Dominion::Card.define_kingdom "Upgrade"
+Dominion::Card.define_kingdom "Wishing Well"
